7e2a25211926b883cc194c03fbd495b06ae36e66
SmallStall <36023330+winuwi@users.noreply.github.com>
2022-03-15 21:22:57 +0900

    first commit

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,25 +5,9 @@ export default function Index() {
       <ul>
         <li>
           <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+            href="test"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+            Test page
           </a>
         </li>
       </ul>
